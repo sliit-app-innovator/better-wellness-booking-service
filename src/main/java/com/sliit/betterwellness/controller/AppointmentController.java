124cf339a0a8f532394ccfdccf06a5e84ee4d35e
dsulochana <damith.sulochana@oloidtechnologies.com>
2025-04-21 13:38:42 +0530

    added logs

--- src/main/java/com/sliit/betterwellness/controller/AppointmentController.java
+++ src/main/java/com/sliit/betterwellness/controller/AppointmentController.java
@@ -5,7 +5,10 @@ import com.sliit.betterwellness.dto.AppointmentViewDTO;
 import com.sliit.betterwellness.dto.CustomerDTO;
 import com.sliit.betterwellness.service.AppointmentService;
 import com.sliit.betterwellness.service.CustomerService;
+import com.sliit.betterwellness.utills.Constants;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,10 +19,9 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/appointment-service")
-@Slf4j
 public class AppointmentController {
 
-
+	private static final Logger log = LogManager.getLogger(AppointmentController.class);
 	private final AppointmentService appointmentService;
 
 	@Autowired
@@ -29,25 +31,33 @@ public class AppointmentController {
 
 	@PostMapping("/appointment")
 	public ResponseEntity<AppointmentDTO> save(@RequestBody AppointmentDTO appointmentDTO, @RequestHeader Map<String, String> headers){
-	//	log.info("Customer save request correlation-id : {}", headers.get(Constants.HEADER_CORRELATION_ID));
-		return ResponseEntity.status(HttpStatus.CREATED).body(appointmentService.save(appointmentDTO));
+		log.info("[IN] Appointment save request correlation-id : {}, Request {}", headers.get(Constants.HEADER_CORRELATION_ID), appointmentDTO);
+		AppointmentDTO response = appointmentService.save(appointmentDTO);
+		log.info("[OUT] Appointment save request correlation-id : {}, Request {}, Response {}", headers.get(Constants.HEADER_CORRELATION_ID), appointmentDTO, response);
+		return ResponseEntity.status(HttpStatus.CREATED).body(response);
 	}
 
 	@GetMapping("/appointment/customer")
 	public ResponseEntity<List<AppointmentViewDTO>> searchCustomerAppointment(@RequestParam(name = "customerId", required = false, defaultValue = "") String customerId, @RequestHeader Map<String, String> headers){
-	//	log.info("Customer search request correlation-id : {}", headers.get(Constants.HEADER_CORRELATION_ID));
-		return ResponseEntity.status(HttpStatus.OK).body(appointmentService.searchCustomerId(customerId != null && !customerId.isEmpty()? Integer.parseInt(customerId): 0));
+		log.info("[IN] Customer search request correlation-id : {}", headers.get(Constants.HEADER_CORRELATION_ID));
+		List<AppointmentViewDTO> response = appointmentService.searchCustomerId(customerId != null && !customerId.isEmpty()? Integer.parseInt(customerId): 0);
+		log.info("[OUT] Customer search request correlation-id : {}", headers.get(Constants.HEADER_CORRELATION_ID));
+		return ResponseEntity.status(HttpStatus.OK).body(response);
 	}
 
 	@GetMapping("/appointment/counsellor")
 	public ResponseEntity<List<AppointmentViewDTO>> searchCounsellorAppointment(@RequestParam(name = "counsellorId", required = false, defaultValue = "") String counsellorId, @RequestHeader Map<String, String> headers){
-	//	log.info("Customer get request correlation-id : {}", headers.get(Constants.HEADER_CORRELATION_ID));
-		return ResponseEntity.status(HttpStatus.OK).body(appointmentService.searchCounsellor(counsellorId != null && !counsellorId.isEmpty()? Integer.parseInt(counsellorId): 0));
+		log.info("[IN] Customer get request correlation-id : {}, counsellorId {} ", headers.get(Constants.HEADER_CORRELATION_ID), counsellorId);
+		List<AppointmentViewDTO> response = appointmentService.searchCounsellor(counsellorId != null && !counsellorId.isEmpty()? Integer.parseInt(counsellorId): 0);
+		log.info("[OUT] Customer get request correlation-id : {}, counsellorId {}, Response {} ", headers.get(Constants.HEADER_CORRELATION_ID), counsellorId, response);
+		return ResponseEntity.status(HttpStatus.OK).body(response);
 	}
 
 	@DeleteMapping("/appointment/{appointmentId}")
 	public ResponseEntity<AppointmentDTO> deleteUser(@PathVariable int appointmentId, @RequestHeader Map<String, String> headers){
-	//	log.info("Customer delete request correlation-id : {}", headers.get(Constants.HEADER_CORRELATION_ID));
-		return ResponseEntity.status(HttpStatus.CREATED).body(appointmentService.delete(appointmentId));
+		log.info("[IN] Customer delete request correlation-id : {}", headers.get(Constants.HEADER_CORRELATION_ID));
+		AppointmentDTO response = appointmentService.delete(appointmentId);
+		log.info("[OUT] Customer delete request correlation-id : {}, appointmentId {}", headers.get(Constants.HEADER_CORRELATION_ID), appointmentId);
+		return ResponseEntity.status(HttpStatus.CREATED).body(response);
 	}
 }
--- src/main/java/com/sliit/betterwellness/controller/AvailabilityController.java
+++ src/main/java/com/sliit/betterwellness/controller/AvailabilityController.java
@@ -5,7 +5,10 @@ import com.sliit.betterwellness.dto.CounsellorAvailabilityDTOs;
 import com.sliit.betterwellness.dto.CounsellorDTO;
 import com.sliit.betterwellness.service.CounsellorAvailabilityService;
 import com.sliit.betterwellness.service.CounsellorService;
+import com.sliit.betterwellness.utills.Constants;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,10 +19,9 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/appointment-service")
-@Slf4j
 public class AvailabilityController {
 
-
+	private static final Logger log = LogManager.getLogger(AvailabilityController.class);
 	private final CounsellorAvailabilityService counsellorAvailabilityService;
 
 	@Autowired
@@ -29,24 +31,29 @@ public class AvailabilityController {
 
 	@PostMapping("/availability")
 	public ResponseEntity<CounsellorAvailabilityDTO> updateUser(@RequestBody CounsellorAvailabilityDTO counsellorAvailabilityDTO, @RequestHeader Map<String, String> headers){
-	//	log.info("Counsellor save request correlation-id : {}", headers.get(Constants.HEADER_CORRELATION_ID));
-		return ResponseEntity.status(HttpStatus.CREATED).body(counsellorAvailabilityService.save(counsellorAvailabilityDTO));
+		log.info("[IN] Counsellor save request correlation-id : {}, Request {}", headers.get(Constants.HEADER_CORRELATION_ID), counsellorAvailabilityDTO);
+		CounsellorAvailabilityDTO response = counsellorAvailabilityService.save(counsellorAvailabilityDTO);
+		log.info("[OUT] Counsellor save request correlation-id : {}, Request {}, Response {}", headers.get(Constants.HEADER_CORRELATION_ID), counsellorAvailabilityDTO, response);
+		return ResponseEntity.status(HttpStatus.CREATED).body(response);
 	}
 
 	@GetMapping("/availability")
 	public ResponseEntity<CounsellorAvailabilityDTOs> search(@RequestParam(name = "counsellorId", required = false, defaultValue = "") String counsellorId,
 																  @RequestParam(name = "date", required = false, defaultValue = "") String date,
 																  @RequestHeader Map<String, String> headers){
-		//	log.info("Counsellor save request correlation-id : {}", headers.get(Constants.HEADER_CORRELATION_ID));
+		log.info("[IN] Counsellor save request correlation-id : {}, counsellorId {} ", headers.get(Constants.HEADER_CORRELATION_ID), counsellorId);
 		CounsellorAvailabilityDTOs counsellorAvailabilityDTOs = new CounsellorAvailabilityDTOs();
 		counsellorAvailabilityDTOs.setSlots(counsellorAvailabilityService.search(Integer.parseInt(counsellorId != null && !counsellorId.isEmpty()? counsellorId : "0"), date));
+		log.info("[OUT] Counsellor save request correlation-id : {}, counsellorId {} Response {}", headers.get(Constants.HEADER_CORRELATION_ID), counsellorId, counsellorAvailabilityDTOs);
 		return ResponseEntity.status(HttpStatus.CREATED).body(counsellorAvailabilityDTOs);
 	}
 
 	@DeleteMapping("/availability/{id}")
 	public ResponseEntity<CounsellorAvailabilityDTO> search(@PathVariable int id,
 																  @RequestHeader Map<String, String> headers){
-		//	log.info("Counsellor save request correlation-id : {}", headers.get(Constants.HEADER_CORRELATION_ID));
-		return ResponseEntity.status(HttpStatus.CREATED).body(counsellorAvailabilityService.delete(id));
+		log.info("[IN] Counsellor save request correlation-id : {}, ID {}", headers.get(Constants.HEADER_CORRELATION_ID), id);
+		CounsellorAvailabilityDTO response = counsellorAvailabilityService.delete(id);
+		log.info("[OUT] Counsellor save request correlation-id : {}, ID {}, response {}", headers.get(Constants.HEADER_CORRELATION_ID), id, response);
+		return ResponseEntity.status(HttpStatus.CREATED).body(response);
 	}
 }
